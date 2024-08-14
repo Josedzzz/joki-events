82fe7ca71766fbc19f64613bef719c8c32ccd321
joki <josedavidamayar@gmail.com>
2024-08-13 23:27:53 -0500

    First commit

---
+++ src/main/java/com/uq/jokievents/JokieventsApplication.java
@@ -0,0 +1,18 @@
+package com.uq.jokievents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokieventsApplication {
+
+	/**
+	 * Starter method for the server
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(JokieventsApplication.class, args);
+	}
+
+
+}
---
+++ src/test/java/com/uq/jokievents/JokieventsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uq.jokievents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokieventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
