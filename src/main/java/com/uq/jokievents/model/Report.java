43dc82d877da50b9e2475c527e9ab13df46d0046
joki <josedavidamayar@gmail.com>
2024-08-15 07:06:21 -0500

    add the report class

---
+++ src/main/java/com/uq/jokievents/model/Report.java
@@ -0,0 +1,58 @@
+package com.uq.jokievents.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "reports")
+public class Report {
+
+    @Id
+    private String id;
+    private String name;
+    private double price;
+    private int maxCapacity;
+
+    // constructor
+    public Report() {}
+
+    public Report(String id, String name, double price, int maxCapacity) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.maxCapacity = maxCapacity;
+    }
+
+    // Getters and setters
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getMaxCapacity() {
+        return maxCapacity;
+    }
+
+    public void setMaxCapacity(int maxCapacity) {
+        this.maxCapacity = maxCapacity;
+    }
+}
