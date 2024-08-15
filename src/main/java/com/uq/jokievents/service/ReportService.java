397e20dc5ee0b929b3b9c6651e860090de43d5af
joki <josedavidamayar@gmail.com>
2024-08-15 09:30:58 -0500

    add the class report service with a basic crud

---
+++ src/main/java/com/uq/jokievents/service/ReportService.java
@@ -0,0 +1,54 @@
+package com.uq.jokievents.service;
+
+import com.uq.jokievents.model.Report;
+import com.uq.jokievents.repository.ReportRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ReportService {
+
+    @Autowired
+    private ReportRepository reportRepository;
+
+    /**
+     * Get a list of all reports from the db
+     *
+     * @return a listof all report objects in the db
+     */
+    public List<Report> findAll() {
+        return reportRepository.findAll();
+    }
+
+    /**
+     * Gets a report by its id from the db
+     *
+     * @param id the identifier of the report
+     * @return an Optional containing the client if found, empty Optional if not
+     */
+    public Optional<Report> findById(String id) {
+        return reportRepository.findById(id);
+    }
+
+    /**
+     * Saves a new report or updates an existing in the db
+     * @param report
+     * @return
+     */
+    public Report save(Report report) {
+        return reportRepository.save(report);
+    }
+
+    /**
+     * Deletes a report from the db using its id
+     *
+     * @param id the identifier of the report to be deleted
+     */
+    public void deleteById(String id) {
+        reportRepository.deleteById(id);
+    }
+
+}
