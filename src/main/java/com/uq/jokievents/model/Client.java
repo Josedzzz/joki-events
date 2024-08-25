3f1fc9efa6139e309248b0faf2fb338ff52e3bf1
dxniel-hit <dancorrea698@gmail.com>
2024-08-25 17:46:03 -0500

    Client class with Lombok

--- src/main/java/com/uq/jokievents/model/Client.java
+++ src/main/java/com/uq/jokievents/model/Client.java
@@ -2,21 +2,23 @@ package com.uq.jokievents.model;
 
 import java.util.ArrayList;
 
+import lombok.Data;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+@Data
 @Document(collection = "clients")
 public class Client {
 
     @Id
-    private String id;
-    private String idCard;
-    private String name;
-    private String direction;
-    private String phoneNumber;
-    private String email;
-    private String password;
+    private String id; // Should it be in dto, or will mongo take care of it?
+    private String idCard; // Kind of a country given identifier. In dto
+    private String name; // In dto
+    private String direction; // Deberías ser "address" pero me da miedo cambiarlo. In dto
+    private String phoneNumber; // In dto
+    private String email; // In dto
+    private String password; // In dto
     private ArrayList<ObjectId> idCoupons;
     private ObjectId idShoppingCart;
     private boolean active;
@@ -37,82 +39,4 @@ public class Client {
         this.idShoppingCart = shoppingCart;
         this.active = false;
     }
-
-    // Getters and setters
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getIdCard() {
-        return idCard;
-    }
-
-    public void setIdCard(String idCard) {
-        this.idCard = idCard;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDirection() {
-        return direction;
-    }
-
-    public void setDirection(String direction) {
-        this.direction = direction;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public ArrayList<ObjectId> getIdCoupons() {
-        return idCoupons;
-    }
-
-    public void setIdCoupons(ArrayList<ObjectId> idCoupons) {
-        this.idCoupons = idCoupons;
-    }
-
-    public ObjectId getIdShoppingCart() {
-        return idShoppingCart;
-    }
-
-    public void setIdShoppingCart(ObjectId idShoppingCart) {
-        this.idShoppingCart = idShoppingCart;
-    }
-
-    public boolean isActive() { return active; }
-
-    public void setActive(boolean active) { this.active = active; }
 }
