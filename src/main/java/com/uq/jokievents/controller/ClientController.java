2a8e09eb84c3e3a7c7197441b2a86a5f1e6db5f4
dxniel-hit <dancorrea698@gmail.com>
2024-08-25 17:47:29 -0500

    Registering client methods

--- src/main/java/com/uq/jokievents/controller/ClientController.java
+++ src/main/java/com/uq/jokievents/controller/ClientController.java
@@ -1,6 +1,8 @@
 package com.uq.jokievents.controller;
 
 import com.uq.jokievents.records.LoginDTO;
+import com.uq.jokievents.records.RegisterClientDTO;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +19,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api/clients")
 public class ClientController {
@@ -27,7 +32,7 @@ public class ClientController {
     /**
      * Get all clients
      *
-     * @return a ResponseEntity objest with containing clients
+     * @return a ResponseEntity object with all the contained clients
      */
     @GetMapping
     public ResponseEntity<?> getAllClients() {
@@ -61,7 +66,7 @@ public class ClientController {
      *
      * @param id the identifier of the client to update
      * @param client the updated client object
-     * @return a ReponseEntity containing the update client
+     * @return a ResponseEntity containing the update client
      */
     @PutMapping("/{id}")
     public ResponseEntity<?> updateClient(@PathVariable String id, @RequestBody Client client) {
@@ -81,12 +86,22 @@ public class ClientController {
 
     /**
      * Login client with email and password
-     *
-     * @param credentials a map containing the email and password
+     * Why return the client dto as an answer
+     * @param body the logindto
      * @return a ResponseEntity containing the client if found, otherwise an error message
      */
     @PostMapping("/login")
     public ResponseEntity<?> loginClient(@RequestBody LoginDTO body) {
         return clientService.findByEmailAndPassword(body.email(), body.password());
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<Map<String, String>> registerClient(@RequestBody RegisterClientDTO rcDto) {
+        // Using the service to register the client
+        Client newClient = clientService.registerNewClient(rcDto);
+        // Returning the client id, for a customized page for the new client!
+        Map<String, String> response = new HashMap<>();
+        response.put("id", newClient.getId());
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 }
