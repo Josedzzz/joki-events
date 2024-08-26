b32cd922d219ee3e3444611fa10165847587002f
juanjarce <juanjtete2004@gmail.com>
2024-08-26 10:01:54 -0500

    @Data dependency implemented for DistributionLocality and deleted lines

--- src/main/java/com/uq/jokievents/model/DistributionLocality.java
+++ src/main/java/com/uq/jokievents/model/DistributionLocality.java
@@ -24,37 +24,4 @@ public class DistributionLocality {
         this.maxCapacity = maxCapacity;
     }
 
-    // Getters and setters
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public int getMaxCapacity() {
-        return maxCapacity;
-    }
-
-    public void setMaxCapacity(int maxCapacity) {
-        this.maxCapacity = maxCapacity;
-    }
 }
