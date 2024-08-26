a61b34b55f7ef6b614e3d1e11afd7ecd9d1131e2
dxniel-hit <dancorrea698@gmail.com>
2024-08-25 19:12:32 -0500

    Verification code logic to Service

--- src/main/java/com/uq/jokievents/controller/ClientController.java
+++ src/main/java/com/uq/jokievents/controller/ClientController.java
@@ -2,6 +2,7 @@ package com.uq.jokievents.controller;
 
 import com.uq.jokievents.records.LoginDTO;
 import com.uq.jokievents.records.RegisterClientDTO;
+import com.uq.jokievents.utils.VerificationService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,12 +13,7 @@ import org.springframework.http.ResponseEntity;
 import com.uq.jokievents.model.Client;
 import com.uq.jokievents.service.ClientService;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -28,6 +24,8 @@ public class ClientController {
 
     @Autowired
     private ClientService clientService;
+    @Autowired
+    private VerificationService verificationService;
 
     /**
      * Get all clients
