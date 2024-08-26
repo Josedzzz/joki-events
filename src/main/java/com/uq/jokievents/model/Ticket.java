ea0ae3116de29dd1e0d5892e9afe4dbebcd8bf80
juanjarce <juanjtete2004@gmail.com>
2024-08-26 10:06:22 -0500

    @Data dependency implemented for Ticket

--- src/main/java/com/uq/jokievents/model/Ticket.java
+++ src/main/java/com/uq/jokievents/model/Ticket.java
@@ -1,9 +1,11 @@
 package com.uq.jokievents.model;
 
+import lombok.Data;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+@Data
 @Document(collection = "tickets")
 public class Ticket {
 
@@ -29,61 +31,4 @@ public class Ticket {
         this.finalPaymentAmount = finalPaymentAmount;
     }
 
-    // Getters and setters
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public ObjectId getIdClient() {
-        return idClient;
-    }
-
-    public void setIdClient(ObjectId idClient) {
-        this.idClient = idClient;
-    }
-
-    public ObjectId getIdCoupon() {
-        return idCoupon;
-    }
-
-    public void setIdCoupon(ObjectId idCoupon) {
-        this.idCoupon = idCoupon;
-    }
-
-    public int getNumTickets() {
-        return numTickets;
-    }
-
-    public void setNumTickets(int numTickets) {
-        this.numTickets = numTickets;
-    }
-
-    public String getLocality() {
-        return locality;
-    }
-
-    public void setLocality(String locality) {
-        this.locality = locality;
-    }
-
-    public Double getNetPaymentAmount() {
-        return netPaymentAmount;
-    }
-
-    public void setNetPaymentAmount(Double netPaymentAmount) {
-        this.netPaymentAmount = netPaymentAmount;
-    }
-
-    public Double getFinalPaymentAmount() {
-        return finalPaymentAmount;
-    }
-
-    public void setFinalPaymentAmount(Double finalPaymentAmount) {
-        this.finalPaymentAmount = finalPaymentAmount;
-    }
 }
