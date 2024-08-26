27a4d7543c5f6eaae9f3183879a404d998989172
juanjarce <juanjtete2004@gmail.com>
2024-08-26 09:58:38 -0500

    @Data dependency implemented for Admin

--- src/main/java/com/uq/jokievents/model/Admin.java
+++ src/main/java/com/uq/jokievents/model/Admin.java
@@ -1,11 +1,13 @@
 package com.uq.jokievents.model;
 
+import lombok.Data;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.ArrayList;
 
+@Data
 @Document(collection = "admins")
 public class Admin {
 
@@ -27,41 +29,4 @@ public class Admin {
         this.active = true;
     }
 
-    // Getters and setters
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public ArrayList<ObjectId> getIdClients() {
-        return idClients;
-    }
-
-    public void setIdClients(ArrayList<ObjectId> idClients) {
-        this.idClients = idClients;
-    }
-
-    public boolean isActive() { return active; }
-
-    public void setActive(boolean active) { this.active = active; }
 }
