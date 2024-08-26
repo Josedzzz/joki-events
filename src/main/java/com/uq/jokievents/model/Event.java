8f3671b42c28b9b0ab7b1dcbae4acf5e069dfd6e
juanjarce <juanjtete2004@gmail.com>
2024-08-26 10:04:05 -0500

    @Data dependency implemented for Event

--- src/main/java/com/uq/jokievents/model/Event.java
+++ src/main/java/com/uq/jokievents/model/Event.java
@@ -1,11 +1,13 @@
 package com.uq.jokievents.model;
 
+import lombok.Data;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Date;
 
+@Data
 @Document(collection = "events")
 public class Event {
 
@@ -35,77 +37,4 @@ public class Event {
         this.imapeDistributionLocality = imapeDistributionLocality;
     }
 
-    // Getters and setters
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public ObjectId getIdDistributionLocality() {
-        return idDistributionLocality;
-    }
-
-    public void setIdDistributionLocality(ObjectId idDistributionLocality) {
-        this.idDistributionLocality = idDistributionLocality;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public Date getEventDate() {
-        return eventDate;
-    }
-
-    public void setEventDate(Date eventDate) {
-        this.eventDate = eventDate;
-    }
-
-    public boolean isAvailablePurchase() {
-        return availablePurchase;
-    }
-
-    public void setAvailablePurchase(boolean availablePurchase) {
-        this.availablePurchase = availablePurchase;
-    }
-
-    public String getImageUrl() {
-        return imageUrl;
-    }
-
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
-
-    public String getImapeDistributionLocality() {
-        return imapeDistributionLocality;
-    }
-
-    public void setImapeDistributionLocality(String imapeDistributionLocality) {
-        this.imapeDistributionLocality = imapeDistributionLocality;
-    }
 }
