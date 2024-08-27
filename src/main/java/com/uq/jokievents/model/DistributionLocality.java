faca3e3a538a759c81bc779180b4acb228fc4bcb
juanjarce <juanjtete2004@gmail.com>
2024-08-27 17:11:41 -0500

    DistributionLocality class modification for correct manipulation

--- src/main/java/com/uq/jokievents/model/DistributionLocality.java
+++ src/main/java/com/uq/jokievents/model/DistributionLocality.java
@@ -13,15 +13,17 @@ public class DistributionLocality {
     private String name;
     private double price;
     private int maxCapacity;
+    private String imapeDistributionLocality;
 
     // constructor
     public DistributionLocality() {}
 
-    public DistributionLocality(String id, String name, double price, int maxCapacity) {
+    public DistributionLocality(String id, String name, double price, int maxCapacity, String imapeDistributionLocality) {
         this.id = id;
         this.name = name;
         this.price = price;
         this.maxCapacity = maxCapacity;
+        this.imapeDistributionLocality = imapeDistributionLocality;
     }
 
 }
