13dce20dd51a5b3a740f52075431fa5912f4cac7
dxniel-hit <dancorrea698@gmail.com>
2024-09-08 23:43:57 -0500

    AdminController adaption

--- src/main/java/com/uq/jokievents/controller/AdminController.java
+++ src/main/java/com/uq/jokievents/controller/AdminController.java
@@ -1,9 +1,16 @@
 package com.uq.jokievents.controller;
 
+import com.uq.jokievents.dtos.AuthAdminDTO;
 import com.uq.jokievents.dtos.LoginClientDTO;
+import com.uq.jokievents.dtos.RecoverPassAdminDTO;
+import com.uq.jokievents.dtos.UpdateAdminDTO;
 import com.uq.jokievents.model.Admin;
 import com.uq.jokievents.model.Client;
-import com.uq.jokievents.service.AdminService;
+import com.uq.jokievents.service.implementation.AdminServiceImpl;
+import com.uq.jokievents.service.interfaces.AdminService;
+
+import lombok.RequiredArgsConstructor;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,77 +19,66 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 @RestController
-@RequestMapping("/api/admins")
+@RequestMapping("/api/admin")
+@RequiredArgsConstructor
 public class AdminController {
 
-    @Autowired
-    private AdminService adminService;
+    private final AdminService adminService;
 
     /**
-     * Gets a list of all admins
-     *
-     * @return a ResponseEntity containing a list of admin objects and an HTTP status of ok
+     * Example JSON:
+     * {
+     *  "username": "XD",
+     *  "email": "mail@mail.com"
+     * }
+     * @param id String
+     * @param dto UpdateAdminDTO
+     * @return ResponseEntity
      */
-    @GetMapping
-    public ResponseEntity<?> getAllAdmins() {
-        return adminService.findAll();
-    }
-
-    /**
-     * Gets an admin by its id
-     *
-     * @param id the identifier
-     * @return a ResponseEntity containing the admin object and an HTTP status of ok if found, otherwise the status is not found
-     */
-    @GetMapping("/{id}")
-    public ResponseEntity<?> getAdminById(@PathVariable String id) {
-        return adminService.findById(id);
-    }
-
-    /**
-     * Create a new admin
-     *
-     * @param admin the admin object to be created
-     * @return a ResponseEntity containing the created Admin
-     */
-    @PostMapping
-    public ResponseEntity<?> createAdmin(@RequestBody Admin admin) {
-        return adminService.create(admin);
-    }
-
-    /**
-     * Updates an existing admin
-     *
-     * @param id the identifier of the admin to be updated
-     * @param admin a ResponseEntity containing the updated admin object and an HTTP status of ok, otherwise not found
-     * @return
-     */
-    @PutMapping("/{id}")
-    public ResponseEntity<?> updateAdmin(@PathVariable String id, @RequestBody Admin admin) {
-        return adminService.update(id, admin);
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> updateAdmin(@PathVariable String id, @Valid @RequestBody UpdateAdminDTO dto) {
+        return adminService.updateAdmin(id, dto);
     }
 
 
-    /**
-     * Deletes an Admin by its identifier
-     *
-     * @param id the identifier of the admin to be deleted
-     * @return a ResponseEntity with an HTTP status of ok if the deletion is succesful
-     */
-    @DeleteMapping("/{id}")
-    public ResponseEntity<?> deleteAdmin(@PathVariable String id) {
-        return adminService.deleteById(id);
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteAdminById(@PathVariable String id) {
+        return adminService.deleteAdminById(id);
     }
-
     /**
-     * Login admin with email and password
-     *
-     * @param body the body with the email and the password
-     * @return a ResponseEntity containing the admin's id if found, otherwise an error message
+     * Example JSON:
+     * {
+     *  "username": "XD",
+     *  "password": "cool-non-encrypted-password"
+     * }
+     * @param dto AuthAdminDTO
+     * @return ResponseEntity
      */
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody LoginClientDTO dto) {
-        return adminService.findByUsernameAndPassword(dto);
+    public ResponseEntity<?> loginAdmin(@Valid @RequestBody AuthAdminDTO dto) {
+        return adminService.loginAdmin(dto);
+    }
+
+    @PostMapping("/send-recover-code")
+    public ResponseEntity<?> sendRecoverCode(@RequestParam String email) {
+        return adminService.sendRecoverPasswordCode(email);
+    }
+
+    /**
+     * Example JSON:
+     * {
+     *  "email": "mail@mail.com",
+     *  "verificationCode": "123456",
+     *  "newPassword": "new-non-encrypted-password"
+     * }
+     * @param dto RecoverPassAdminDTO
+     * @return ResponseEntity
+     */
+    @PostMapping("/recover-password")
+    public ResponseEntity<?> recoverPassword(@Valid @RequestBody RecoverPassAdminDTO dto) {
+        return adminService.recoverPassword(dto);
     }
 }
