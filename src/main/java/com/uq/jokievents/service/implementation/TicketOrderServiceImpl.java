95fdeff9c9abc48dabd85b48c29f419b6739fcea
juanjarce <juanjtete2004@gmail.com>
2024-09-09 10:15:25 -0500

    Ticket Order Service & Implementation

--- src/main/java/com/uq/jokievents/service/implementation/TicketOrderServiceImpl.java
+++ src/main/java/com/uq/jokievents/service/implementation/TicketOrderServiceImpl.java
@@ -1,7 +1,8 @@
-package com.uq.jokievents.service;
+package com.uq.jokievents.service.implementation;
 
 import com.uq.jokievents.model.TicketOrder;
 import com.uq.jokievents.repository.TicketOrderRepository;
+import com.uq.jokievents.service.interfaces.TicketOrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,7 +12,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public class TicketOrderService {
+public class TicketOrderServiceImpl implements TicketOrderService {
 
     @Autowired
     private TicketOrderRepository ticketOrderRepository;
@@ -107,3 +108,4 @@ public class TicketOrderService {
     }
 
 }
+
