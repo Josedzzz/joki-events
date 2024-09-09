adfaf1c416f24a25b520e88e2562ddf6a103e685
dxniel-hit <dancorrea698@gmail.com>
2024-09-08 23:42:06 -0500

    New Admin.java model class atributtes

--- src/main/java/com/uq/jokievents/model/Admin.java
+++ src/main/java/com/uq/jokievents/model/Admin.java
@@ -2,6 +2,7 @@ package com.uq.jokievents.model;
 
 import lombok.Data;
 import org.bson.types.ObjectId;
+import java.time.LocalDateTime;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -13,16 +14,20 @@ public class Admin {
 
     @Id
     private String id;
+    private String email;
     private String username;
     private String password;
+    private String verificationCode;
+    private LocalDateTime verificationCodeExpiration;
     private ArrayList<ObjectId> idClients;
     private boolean active;
 
     // Constructor
     public Admin() {}
 
-    public Admin(String id, String username, String password, ArrayList<ObjectId> idClients) {
+    public Admin(String id, String email,String username, String password, ArrayList<ObjectId> idClients) {
         this.id = id;
+        this.email = email;
         this.username = username;
         this.password = password;
         this.idClients = idClients;
