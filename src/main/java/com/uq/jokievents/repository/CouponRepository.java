983d6dae09e806b0ad542163aedade96066eb729
dxniel.hit <dancorrea698@gmail.com>
2024-09-10 10:20:54 -0500

    New CouponRepository.java method

--- src/main/java/com/uq/jokievents/repository/CouponRepository.java
+++ src/main/java/com/uq/jokievents/repository/CouponRepository.java
@@ -1,9 +1,12 @@
 package com.uq.jokievents.repository;
 
 import com.uq.jokievents.model.Coupon;
+import java.util.Optional;
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CouponRepository extends MongoRepository<Coupon, String> {
+    Optional<Coupon> findById(ObjectId id);
 }
