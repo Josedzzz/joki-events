12af9067ab14b401ecb82305917e27a335fbefd5
juanjarce <juanjarce@gmail.com>
2024-09-10 10:18:18 -0500

    Deleted import

--- src/main/java/com/uq/jokievents/JokieventsApplication.java
+++ src/main/java/com/uq/jokievents/JokieventsApplication.java
@@ -4,8 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 public class JokieventsApplication {
