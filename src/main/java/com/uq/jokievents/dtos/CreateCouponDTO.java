50d168016af7eaf651424dbe99821a34ce41a528
juanjarce <juanjtete2004@gmail.com>
2024-09-10 21:55:03 -0500

    Coupons dto class to record

--- src/main/java/com/uq/jokievents/dtos/CreateCouponDTO.java
+++ src/main/java/com/uq/jokievents/dtos/CreateCouponDTO.java
@@ -1,26 +1,25 @@
 package com.uq.jokievents.dtos;
 
 import java.time.LocalDateTime;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
-import javax.validation.constraints.*;
+public record CreateCouponDTO(
+        @NotBlank(message = "Name cannot be blank")
+        String name,
 
-import lombok.Data;
+        @NotNull(message = "Discount cannot be null")
+        @Min(value = 0, message = "Discount must be greater than or equal to 0")
+        @Max(value = 100, message = "Discount must be less than or equal to 100")
+        Double discount,
 
-@Data
-public class CreateCouponDTO {
+        @NotNull(message = "Expiration date cannot be null")
+        LocalDateTime expirationDate,
 
-    @NotBlank(message = "Name cannot be blank")
-    private String name;
+        @NotNull(message = "Minimum purchase amount cannot be null")
+        @Min(value = 0, message = "Minimum purchase amount must be greater than or equal to 0")
+        Double minPurchaseAmount
+) {}
 
-    @NotNull(message = "Discount cannot be null")
-    @Min(value = 0, message = "Discount must be greater than or equal to 0")
-    @Max(value = 100, message = "Discount must be less than or equal to 100")
-    private Double discount;
-
-    @NotNull(message = "Expiration date cannot be null")
-    private LocalDateTime expirationDate;
-
-    @NotNull(message = "Minimum purchase amount cannot be null")
-    @Min(value = 0, message = "Minimum purchase amount must be greater than or equal to 0")
-    private Double minPurchaseAmount;
-}
