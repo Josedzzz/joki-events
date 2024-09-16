259e4f341006d624c590ac45dee4d168b3fb6b84
juanjarce <juanjtete2004@gmail.com>
2024-09-15 23:49:58 -0500

    Classes aimed to security

---
+++ src/main/java/com/uq/jokievents/utils/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.uq.jokievents.utils;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+
+// TODO Read this: https://spring.io/blog/2022/02/21/spring-security-without-the-websecurityconfigureradapter
+// Whoever understands this part may do it
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig  {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(withDefaults());
+        return http.build();
+    }
+}
