474c24fa6b09531646d6b5c3b0dab8772bd78fea
juanjarce <juanjtete2004@gmail.com>
2024-09-16 19:14:24 -0500

    New SecurityConfig method

--- src/main/java/com/uq/jokievents/utils/SecurityConfig.java
+++ src/main/java/com/uq/jokievents/utils/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.uq.jokievents.utils;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
@@ -24,5 +26,10 @@ public class SecurityConfig  {
                 )
                 .httpBasic(withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
