9fd16da2f34029f19c58d8c5283de370a59b6769
juanjarce <juanjtete2004@gmail.com>
2024-09-22 22:25:51 -0500

    firebase config

---
+++ src/main/java/com/uq/jokievents/config/FirebaseConfig.java
@@ -0,0 +1,31 @@
+package com.uq.jokievents.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@Configuration
+public class FirebaseConfig {
+
+    @Bean
+    public FirebaseApp initializeFirebase() throws IOException {
+        FileInputStream serviceAccount = new FileInputStream(
+                "src/main/resources/joki-events-img-repo-firebase-adminsdk-k2dzt-5632c3e6b6.json"
+        );
+
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .setStorageBucket("gs://joki-events-img-repo.appspot.com")
+                .build();
+
+        if(FirebaseApp.getApps().isEmpty()) {
+            return FirebaseApp.initializeApp(options);
+        }
+        return null;
+    }
+}
