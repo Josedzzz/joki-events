368ee9119177834fab0ca634f592b2bb96e084f5
juanjarce <juanjtete2004@gmail.com>
2024-10-10 10:57:46 -0500

    Code verification after registering on ClientController

--- src/main/java/com/uq/jokievents/service/interfaces/ClientService.java
+++ src/main/java/com/uq/jokievents/service/interfaces/ClientService.java
@@ -19,4 +19,6 @@ public interface ClientService {
     ResponseEntity<?> cancelLocalityOrder(String clientId, LocalityOrderAsClientDTO dto);
     ResponseEntity<?> loadShoppingCart(String clientId);
     ResponseEntity<?> applyCoupon(String clientId, String coupon);
+
+    ResponseEntity<?> verifyClient(String clientId, String verificationCode);
 }
