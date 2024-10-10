2619ba536572ba2ad9b3c5a6f3ecf5bba80039ff
dxniel-hit <dancorrea698@gmail.com>
2024-10-09 22:53:32 -0500

    Updated dataset.js

--- src/test/resources/dataset.js
+++ src/test/resources/dataset.js
@@ -1,4 +1,4 @@
-db = connect('mongodb://localhost:27017/jokievents');
+db = connect('mongodb://localhost:27017/jokievents-testing');
 
 // This has to be updated
 
@@ -47,56 +47,115 @@ db.clients.insertMany([
         "_class": "com.uq.jokievents.model.Client",
         "address": "quimbaya"
     },
-    {
-        idCard: "87654321B",
-        name: "Bob Builder",
-        address: "456 Construction St, Buildtown",
-        phoneNumber: "555-5678",
-        email: "bob@example.com",
-        password: "securepassword2",
-        idShoppingCart: "shopping_cart_2",
-        active: false,
-        verificationCode: "XYZ789",
-        verificationCodeExpiration: new Date("2024-11-30T23:59:59"),
-        role: "CLIENT"
+        {
+            "_id": {
+                "$oid": "6706a5101654657267419fef"
+            },
+            "idCard": "AB123456",
+            "name": "new-name-n3on431408",
+            "address": "Casa de 431408",
+            "phoneNumber": "3141431408",
+            "email": "greatEmail431408@example.com",
+            "password": "$2a$10$0xqYdGWwT7vnz3COTlUeje/1gykjDOasab8rjzm16jeEbsX60TRZu",
+            "idShoppingCart": "6706afa4ba774f520afb4ca2",
+            "active": false,
+            "verificationCode": "390209",
+            "verificationCodeExpiration": {
+                "$date": "2024-10-10T03:04:54.417Z"
+            },
+            "role": "CLIENT",
+            "_class": "com.uq.jokievents.model.Client"
     },
-    {
-        idCard: "12348765C",
-        name: "Charlie Brown",
-        address: "789 Comic Blvd, Snoopyville",
-        phoneNumber: "555-6789",
-        email: "charlie@example.com",
-        password: "securepassword3",
-        idShoppingCart: "shopping_cart_3",
-        active: true,
-        verificationCode: "LMN456",
-        verificationCodeExpiration: new Date("2024-10-15T23:59:59"),
-        role: "CLIENT"
-    },
-    {
-        idCard: "87651234D",
-        name: "Dorothy Gale",
-        address: "123 Oz St, Emerald City",
-        phoneNumber: "555-9876",
-        email: "dorothy@example.com",
-        password: "securepassword4",
-        idShoppingCart: "shopping_cart_4",
-        active: true,
-        verificationCode: "QRS852",
-        verificationCodeExpiration: new Date("2024-09-30T23:59:59"),
-        role: "CLIENT"
-    },
-    {
-        idCard: "65432198E",
-        name: "Eve Adams",
-        address: "321 Paradise Rd, Eden",
-        phoneNumber: "555-4321",
-        email: "eve@example.com",
-        password: "securepassword5",
-        idShoppingCart: "shopping_cart_5",
-        active: false,
-        verificationCode: "TUV963",
-        verificationCodeExpiration: new Date("2024-08-31T23:59:59"),
-        role: "CLIENT"
+        {
+            "_id": {
+                "$oid": "66f3b71c95dcb9591580d078"
+            },
+            "idCard": "1097032971",
+            "name": "joki",
+            "address": "quimbaya",
+            "phoneNumber": "1231231234",
+            "email": "balinius11@gmail.com",
+            "password": "$2a$10$GZODpgapTfxgrDS167XnrOJRFXGBQ4m8q6ZFdOhc36wWxAIgpirTq",
+            "idShoppingCart": "66f3b71795dcb9591580d077",
+            "active": true,
+            "role": "CLIENT",
+            "_class": "com.uq.jokievents.model.Client"
     }
-]);
+],
+
+db.coupons.insertMany [
+    {
+        "_id": {
+            "$oid": "67054eb8734d84764f8b0316"
+        },
+        "name": "382124133444",
+        "discountPercent": 25,
+        "expirationDate": {
+            "$date": "2025-01-01T04:59:59.000Z"
+        },
+        "minPurchaseAmount": 150,
+        "isUsed": false,
+        "_class": "com.uq.jokievents.model.Coupon"
+    }
+],
+
+db.events.insertMany [
+    {
+        "_id": {
+            "$oid": "6705590b39e3c64472be8665"
+        },
+        "name": "Updated Event Name 726643838294",
+        "address": "Updated City",
+        "city": "123 Updated Address",
+        "eventDate": {
+            "$date": "2024-10-09T16:42:54.098Z"
+        },
+        "availableForPurchase": true,
+        "localities": [
+            {
+                "name": "ULTRA VIP726643838294",
+                "price": 100,
+                "maxCapacity": 50,
+                "currentOccupancy": 0
+            }
+        ],
+        "totalAvailablePlaces": 100,
+        "eventImageUrl": "https://firebasestorage.googleapis.com/v0/b/joki-events-img-repo.appspot.com/o/a2d8f062-66c1-4cca-8da4-016be0d32e60-s.png?alt=media",
+        "localitiesImageUrl": "https://firebasestorage.googleapis.com/v0/b/joki-events-img-repo.appspot.com/o/a17b040e-be26-4b98-af33-da0a7885bb31-s.png?alt=media",
+        "eventType": "CONCERT",
+        "_class": "com.uq.jokievents.model.Event"
+    },
+
+    {
+        "_id": {
+            "$oid": "67072eac29bb6b12ddff0f9d"
+        },
+        "name": "Great Feid Music Concert",
+        "address": "123 Main Street",
+        "city": "New York",
+        "eventDate": {
+            "$date": "2024-12-16T01:00:00.000Z"
+        },
+        "availableForPurchase": true,
+        "localities": [
+            {
+                "name": "VIP",
+                "price": 200,
+                "maxCapacity": 100,
+                "currentOccupancy": 0
+            },
+            {
+                "name": "General Admission",
+                "price": 50,
+                "maxCapacity": 4890,
+                "currentOccupancy": 0
+            }
+        ],
+        "totalAvailablePlaces": 5000,
+        "eventImageUrl": "https://firebasestorage.googleapis.com/v0/b/joki-events-img-repo.appspot.com/o/8e9e6269-60e0-41b6-b190-5f55cf18bd87-s.png?alt=media",
+        "localitiesImageUrl": "https://firebasestorage.googleapis.com/v0/b/joki-events-img-repo.appspot.com/o/d67942ee-4377-4d39-9102-3000ec09a0e4-s.png?alt=media",
+        "eventType": "CONCERT",
+        "_class": "com.uq.jokievents.model.Event"
+    }
+]
+);
