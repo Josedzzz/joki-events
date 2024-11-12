08418ba783d129ec59dcdbf229a66058b1722a6c
dxniel-hit <dancorrea698@gmail.com>
2024-11-11 20:33:52 -0500

    a lot

--- src/main/java/com/uq/jokievents/config/ApplicationConfig.java
+++ src/main/java/com/uq/jokievents/config/ApplicationConfig.java
@@ -17,6 +17,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
